68f62b5caf7f3af2be1dbb971e698b9eb6540631
tobiodp <thungo3011@gmail.com>
2025-07-14 21:35:10 +0700

    afafasdad

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-
-
 // //dùng fetch
 // fetch('https://65300e576c756603295e2eec.mockapi.io/Items') // tuần tự fetch-> then(...) -> catch ( -> finally(...))
 //     .then(function (response) { // nếu kh lỗi thì thực hiện cái này
-//         return response.json(); //parse: dịch/ mã hóa ra dưới dạng json {key: value} 
+//         return response.json(); //parse: dịch/ mã hóa ra dưới dạng json {key: value}
 //     })
 //     .then(function (data) {
 //         console.log('Data received:', data);
@@ -15,39 +13,41 @@
 //         console.log('luôn chạy')
 //     });
 
-
 // // async await : xử lí bất đồng bộ (code kh chạy theo trình từ trên xuống dưới )
-// async function getItems() { 
+// async function getItems() {
 //     try {
 //         const response = await fetch('https://65300e576c756603295e2eec.mockapi.io/Items'); //3s nhận data từ server
 //         const data = await response.json(); // 2s
-//         console.log('Data received:', data); 
+//         console.log('Data received:', data);
 //     } catch (error) {
 //         console.error('Fetch error:', error);
 //     }
 // }
 // getOrders();
 
+// cccvbcvbcvbvcbvcbcvbcvbvcbvbcvbvcbcvbcvbvcbcvbvcbb
+const newItem = {
+  name: "Ngọc Thu",
+  price: "anh minh",
+  describe: "Đại giỏi",
+  category: "bottle",
+};
 
-const newItem =  {
-    name: "Ngọc Thu",
-    price: "anh minh",
-    describe: "Đại giỏi",
-    category: "bottle"
-  }
-
-fetch('https://65300e576c756603295e2eec.mockapi.io/Items', {
-  method: 'POST',
-  headers: {'content-type':'application/json'}, // Send your data in the request body as JSON
-  body: JSON.stringify(newItem)
-}).then(res => {
-  if (res.ok) {
-    console.log("up len thanh cong")
-      return res.json();
-  }
-  // handle error
-}).then(task => {
-  // do something with the new task
-}).catch(error => {
-  // handle error
+fetch("https://65300e576c756603295e2eec.mockapi.io/Items", {
+  method: "POST",
+  headers: { "content-type": "application/json" }, // Send your data in the request body as JSON
+  body: JSON.stringify(newItem),
 })
+  .then((res) => {
+    if (res.ok) {
+      console.log("up len thanh cong");
+      return res.json();
+    }
+    // handle error
+  })
+  .then((task) => {
+    // do something with the new task
+  })
+  .catch((error) => {
+    // handle error
+  });
